28a11d34983793c72d9315a3da0b427aca0cc901
bia-amorim <biia.a.matos@gmail.com>
2020-11-29 21:09:22 -0300

    first commit

---
+++ src/main/java/br/edu/usj/ads/lpii/calculadoraspringboot/CalculadoraSpringbootApplication.java
@@ -0,0 +1,13 @@
+package br.edu.usj.ads.lpii.calculadoraspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraSpringbootApplication.class, args);
+	}
+
+}
